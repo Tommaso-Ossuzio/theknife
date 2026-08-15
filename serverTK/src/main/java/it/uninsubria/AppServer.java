2c6fd0e6d3db59f6150ac4d8ad8ccba919e79a66
elia_toschi <eliatoschi.24@gmail.com>
2026-08-15 10:49:04 +0200

    Aggiunta della libreria flyway per creare il database e le sue configurazioni. Aggiunta del file properties.example che andrà modificato da tutti con le proprie credenziali

--- serverTK/src/main/java/it/uninsubria/AppServer.java
+++ serverTK/src/main/java/it/uninsubria/AppServer.java
@@ -1,13 +1,28 @@
 package it.uninsubria;
 
-/**
- * Hello world!
- *
- */
-public class AppServer
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.FlywayException;
+
+public class AppServer {
+    public static void main(String[] args) {
+        System.out.println("Avvio del Server TheKnife...");
+
+        DatabaseConfig.createDatabaseIfMissing();
+
+        try {
+            Flyway flyway = Flyway.configure()
+                    .dataSource(DatabaseConfig.getTargetUrl(), DatabaseConfig.getUser(), DatabaseConfig.getPassword())
+                    .load();
+            //legge il file
+            flyway.migrate();
+
+            System.out.println("Tabelle pronte nel database theknife_db!");
+
+        } catch (FlywayException e) {
+            System.err.println("Errore durante la configurazione del Database:");
+            e.printStackTrace();
+        }
+
+        // Qui sotto il codice per avviare il Server Socket
     }
 }
