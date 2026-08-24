290819f354875799591038dc26d1d7ae2fcfd4cd
vis3ee <visellimichele2@gmail.com>
2026-08-24 12:39:25 +0200

    Creato DTO autenticazione con email e password

---
+++ commonTK/src/main/java/it/uninsubria/dto/AuthDTO.java
@@ -0,0 +1,45 @@
+package it.uninsubria.dto;
+
+import java.io.Serializable;
+
+/**
+ * Dati utilizzati dal client per la richiesta di autenticazione ({@code LOG}).
+ *
+ * @author Michele Viselli
+ */
+public class AuthDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String email;
+    private String password;
+
+    public AuthDTO(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "AuthDTO{" +
+                "email='" + email + '\'' +
+                ", password='[PROTECTED]'" +
+                '}';
+    }
+}
