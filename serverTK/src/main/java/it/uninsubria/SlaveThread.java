4b716452522c5a46a36839322112b0ffb39138cc
Cele-stino <celestino.resteghini@gmail.com>
2026-08-25 21:36:12 +0200

    Aggiunta stampe nello SlaveThread

--- serverTK/src/main/java/it/uninsubria/SlaveThread.java
+++ serverTK/src/main/java/it/uninsubria/SlaveThread.java
@@ -38,28 +38,35 @@ public class SlaveThread extends Thread {
                 if (comando.equals("END")) break;
                 if (comando.equals("LOG")) {
                     AuthDTO credenziali = (AuthDTO) in.readObject();
+                    System.out.println("LOG: ricevuto: " + credenziali.toString());
                     //TODO controllo se le credenziali sono corrette
                     if(true) { //se il controllo è andato a buon fine
+                        System.out.println("LOG: invio: true");
                         out.writeObject(true);
                         out.flush();
                     } else {
+                        System.out.println("LOG: invio: false");
                         out.writeObject(false);
                         out.flush();
                     }
                 }
                 if (comando.equals("REG")) {
                     UtenteDTO nuovoUtente = (UtenteDTO) in.readObject();
+                    System.out.println("REG: ricevuto: " + nuovoUtente.toString());
                     //TODO controllare nel db che l'utente non sia già presente e nel caswo registrarlo
                     if(true) { //se è avvenuta la registrazione
+                        System.out.println("REG: invio: true");
                         out.writeObject(true);
                         out.flush();
                     } else {
+                        System.out.println("REG: invio: false");
                         out.writeObject(false); //se esiste già
                         out.flush();
                     }
                 }
                 if(comando.equals("FILTRO")) {
                     FiltroRistoranteDTO specificheFiltro = (FiltroRistoranteDTO) in.readObject();
+                    System.out.println("FILTRO: ricevuto: " + specificheFiltro.toString());
                     LinkedList<RistoranteDTO> ristoranti;
 
                     try (Connection conn = DriverManager.getConnection(
@@ -77,42 +84,53 @@ public class SlaveThread extends Thread {
                         ristoranti = new LinkedList<>();
                     }
 
+                    System.out.println("FILTRO: invio: lista ristoranti");
                     out.writeObject(ristoranti);
                     out.flush();
                 }
                 if(comando.equals("REC")) {
                     int idRistorante = (int) in.readObject();
+                    System.out.println("REC: ricevuto: " + idRistorante);
                     //TODO prendere dal db tutte le recensioni relative all'id del ristorante passato dal client
                     LinkedList<RecensioneDTO> recensioni = new LinkedList<>(); //TODO sostituire la lista vuota con la lista delle recensioni richieste
+                    System.out.println("REC: invio: lista recensioni");
                     out.writeObject(recensioni);
                     out.flush();
                 }
                 if(comando.equals("AGG-REC")) {
                     RecensioneDTO recensione = (RecensioneDTO) in.readObject();
+                    System.out.println("AGG-REC: ricevuto: " + recensione.toString());
                     //TODO aggiungere recensione nel db
                 }
                 if(comando.equals("VIS-REC")) {
                     int idUtente = (int) in.readObject();
+                    System.out.println("VIS-REC: ricevuto: " + idUtente);
                     //TODO prendere dal db tutte le recensioni relative all'id dell'utente passato dal client
                     LinkedList<RecensioneDTO> recensioni = new LinkedList<>(); //TODO sostituire la lista vuota con la lista delle recensioni richieste
+                    System.out.println("VIS-REC: invio: lista recensioni");
                     out.writeObject(recensioni);
                     out.flush();
                 }
                 if(comando.equals("MOD-REC")) {
                     RecensioneDTO recensione = (RecensioneDTO) in.readObject();
+                    System.out.println("MOD-REC: ricevuto: " + recensione.toString());
                     //TODO modificare nel db la recensione con i dati forniti dal client
                     LinkedList<RecensioneDTO> recensioni = new LinkedList<>(); //TODO sostituire la lista vuota con la lista delle recensioni richieste
+                    System.out.println("MOD-REC: invio: lista recensioni");
                     out.writeObject(recensioni);
                     out.flush();
                 }
                 if(comando.equals("ELIM-REC")) {
-                    RecensioneDTO recensione = (RecensioneDTO) in.readObject();
+                    int idRecensione = (int) in.readObject();
+                    System.out.println("ELIM-REC: ricevuto: " + idRecensione);
                     //TODO eliminare nel db la recensione con i dati forniti dal client
                 }
                 if(comando.equals("VIS-PREF")) {
                     int idUtente = (int) in.readObject();
+                    System.out.println("VIS-PREF: ricevuto: " + idUtente);
                     //TODO prendere dal db tutti i ristoranti preferiti del client
                     LinkedList<RistoranteDTO> ristoranti = new LinkedList<>(); //TODO sostituire la lista vuota con la lista dei ristoranti richiesti
+                    System.out.println("VIS-PREF: invio: lista ristoranti");
                     out.writeObject(ristoranti);
                     out.flush();
                 }
@@ -127,6 +145,7 @@ public class SlaveThread extends Thread {
                     HashMap<String, Integer> id = (HashMap<String, Integer>) in.readObject();
                     int idUtente = id.get("idUtente");
                     int idRistorante = id.get("idRistorante");
+                    System.out.println("ELIM-PREF: ricevuto: " + idUtente + " " + idRistorante);
                     //TODO eliminare il ristorante dai preferiti
 
                 }
@@ -134,41 +153,52 @@ public class SlaveThread extends Thread {
                     HashMap<String, Integer> id = (HashMap<String, Integer>) in.readObject();
                     int idUtente = id.get("idUtente");
                     int idRistorante = id.get("idRistorante");
+                    System.out.println("AGG-PREF: ricevuto: " + idUtente + " " + idRistorante);
                     //TODO aggiungere il ristorante ai preferiti
                 }
                 if(comando.equals("MAPS")) {
                     int idRistorante = (int) in.readObject();
+                    System.out.println("MAPS: ricevuto: " + idRistorante);
                     //TODO cercare coordinate del ristorante passato dal client
                     CoordinateDTO coordinate= new CoordinateDTO(1, 1,1); //TODO sostituire le coordinate con quelle prese dal db
+                    System.out.println("MAPS: invio: " + coordinate.toString());
                     out.writeObject(coordinate);
                     out.flush();
                 }
                 if(comando.equals("RIST")) {
                     int idRistoratore = (int) in.readObject();
+                    System.out.println("RIST: ricevuto: " + idRistoratore);
                     //TODO prendere dal db tutti i ristoranti associati al ristoratore
                     LinkedList<RistoranteDTO> ristoranti = new LinkedList<>(); //TODO sostituire la lista vuota con la lista dei ristoranti richiesti
+                    System.out.println("RIST: invio: lista ristoranti");
                     out.writeObject(ristoranti);
                     out.flush();
                 }
                 if(comando.equals("AGG-RIST")) {
                     RistoranteDTO ristorante = (RistoranteDTO) in.readObject();
+                    System.out.println("AGG-RIST: ricevuto: " + ristorante.toString());
                     //TODO aggiungere il ristorante nel db associandolo al ristoratore
                     LinkedList<RistoranteDTO> ristoranti = new LinkedList<>(); //TODO sostituire la lista vuota con la lista dei ristoranti richiesti
+                    System.out.println("AGG-RIST: invio: lista ristoranti");
                     out.writeObject(ristoranti);
                     out.flush();
                 }
                 if(comando.equals("REC-NO-RISP")) {
                     int idRistoratore = (int) in.readObject();
+                    System.out.println("REC-NO-RISP: ricevuto: " + idRistoratore);
                     //TODO cercare nel db le recensioni senza risposta relative ai ristoranti del ristoratore
                     LinkedList<RecensioneDTO> recensioni = new LinkedList<>(); //TODO sostituire la lista vuota con la lista delle recensioni richieste
+                    System.out.println("REC-NO-RISP: invio: lista recensioni");
                     out.writeObject(recensioni);
                     out.flush();
                 }
                 if(comando.equals("RISP-REC")) {
                     RecensioneDTO recensione = (RecensioneDTO) in.readObject();
+                    System.out.println("RISP-REC: ricevuto: " + recensione.toString());
                     //TODO aggiungere nel db la risposta alla recensione
                 }
             }
+            System.out.println("END");
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         } finally {
